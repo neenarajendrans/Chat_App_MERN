f5fddf4cec685ed78cc9407b05c4406b7b069773
Neena <neenarajendrans@gmail.com>
2024-07-20 18:44:50 +0530

    Third Commit test

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,10 @@
-import User from '../models/user.model.js'
 import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
-export const signup = async (req,res)=>{
-    try{
-        const { fullName, username, password, confirmPassword, gender } = req.body;
+export const signup = async (req, res) => {
+	try {
+		const { fullName, username, password, confirmPassword, gender } = req.body;
 
 		if (password !== confirmPassword) {
 			return res.status(400).json({ error: "Passwords don't match" });
@@ -32,8 +32,8 @@ export const signup = async (req,res)=>{
 			gender,
 			profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
 		});
-        
-        if (newUser) {
+
+		if (newUser) {
 			// Generate JWT token here
 			generateTokenAndSetCookie(newUser._id, res);
 			await newUser.save();
@@ -47,16 +47,15 @@ export const signup = async (req,res)=>{
 		} else {
 			res.status(400).json({ error: "Invalid user data" });
 		}
-
-    }catch (error) {
+	} catch (error) {
 		console.log("Error in signup controller", error.message);
 		res.status(500).json({ error: "Internal Server Error" });
 	}
-}
+};
+
 export const login = async (req, res) => {
 	try {
 		const { username, password } = req.body;
-		console.log(req.body);
 		const user = await User.findOne({ username });
 		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
@@ -87,4 +86,3 @@ export const logout = (req, res) => {
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
-
